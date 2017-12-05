41a92da7e8937a1654eaba36e7c8d75d919e753d
Dukecz <stukov0210@gmail.com>
2017-12-05 20:00:00 +0100

    removed unnecessary variable from 2017 day 4

--- 2017/day4/4b.php
+++ 2017/day4/4b.php
@@ -3,8 +3,6 @@
 $input = fopen('php://stdin', 'rb');
 $sum = 0;
 
-$string = "abcde xyz ecdabr";
-
 while(!empty(trim($line = fgets($input)))) {
     $line = trim($line);
     $line = explode(" ", $line);
