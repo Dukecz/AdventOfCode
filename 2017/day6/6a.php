3e74683d8d7035dd284c5013ec70e0ad578e3f1f
Dukecz <stukov0210@gmail.com>
2017-12-07 13:20:45 +0100

    2017 day 6

---
+++ 2017/day6/6a.php
@@ -0,0 +1,33 @@
+<?php
+
+$input = "";
+
+$input = explode("\t", $input);
+
+$seen = [];
+$steps = 0;
+
+while(!in_array($input, $seen)) {
+	$seen[] = $input;
+	$max = 0;
+	$location = 0;
+	foreach($input as $key => $bank) {
+		if ($bank > $max) {
+			$max = $bank;
+			$location = $key;
+		}
+	}
+	
+	$input[$location] = 0;
+	$location++;
+	$i = 0;
+	while ($max > 0) {
+		$key = ($i + $location) % (count($input));
+		$input[$key]++;
+		--$max;
+		++$location;
+	}
+	$steps++;
+}
+
+echo $steps . "\n";
