d58d05a671b2cb883305b1e7e5b0cbb369b5e313
Dukecz <stukov0210@gmail.com>
2018-12-15 01:07:09 +0100

    2018 day 8 part 1

---
+++ 2018/day8/8a.php
@@ -0,0 +1,29 @@
+<?php
+
+// real is too long for stdin
+$input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2';
+
+$input = explode(' ', $input);
+$input = array_map(function($value) {return (int) $value;}, $input);
+
+function getMetadata(&$input)
+{
+	$nodes = array_shift($input);
+	$numberOfMetadata = array_shift($input);
+	$sum = 0;
+
+	for ($i = 1; $i <= $nodes; ++$i) {
+		$sum += getMetadata($input);
+	}
+
+	if ($numberOfMetadata > 0) {
+		for ($i = 1; $i <= $numberOfMetadata; ++$i) {
+			$metadata = array_shift($input);
+			$sum += $metadata;
+		}
+	}
+
+	return $sum;
+}
+
+echo sprintf('Day 8 Part One result: %d' . PHP_EOL, getMetadata($input));
