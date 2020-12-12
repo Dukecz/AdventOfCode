9d023a13404f61a2778daff14ff772588150b808
Dukecz <stukov0210@gmail.com>
2020-12-12 18:53:21 +0100

    fixed output text

--- 2020/day1/1b.php
+++ 2020/day1/1b.php
@@ -17,7 +17,7 @@ for ($i = 0; $i < $dataCount - 2; ++$i) {
 	for($j = $i + 1; $j < $dataCount - 1; ++$j) {
 		for($k = $j + 1; $k < $dataCount; ++$k) {
 			if ($data[$i] + $data[$j] + $data[$k] === 2020) {
-				echo sprintf('Day 1 Part One result: %d * %d * %d = %d' . PHP_EOL, $data[$i], $data[$j], $data[$k], $data[$i] * $data[$j] * $data[$k]);
+				echo sprintf('Day 1 Part Two result: %d * %d * %d = %d' . PHP_EOL, $data[$i], $data[$j], $data[$k], $data[$i] * $data[$j] * $data[$k]);
 			}
 		}
 	}
