e338b1014409d6d37a1e28f2b94b5b18b5447179
Dukecz <stukov0210@gmail.com>
2020-12-12 18:41:54 +0100

    2020 day 1 part 2

---
+++ 2020/day1/1b.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = fopen('php://stdin', 'rb');
+
+$data = [];
+
+while(!empty(trim($line = fgets($input)))) {
+	$data[] = (int) trim($line);
+}
+
+sort($data);
+
+$result = 0;
+$dataCount = count($data);
+
+for ($i = 0; $i < $dataCount - 2; ++$i) {
+	for($j = $i + 1; $j < $dataCount - 1; ++$j) {
+		for($k = $j + 1; $k < $dataCount; ++$k) {
+			if ($data[$i] + $data[$j] + $data[$k] === 2020) {
+				echo sprintf('Day 1 Part One result: %d * %d * %d = %d' . PHP_EOL, $data[$i], $data[$j], $data[$k], $data[$i] * $data[$j] * $data[$k]);
+			}
+		}
+	}
+}
